e60dff7e498de9a9dbe1c08b4b640dd7306dc966
Tom Wilson <tom@jackhq.com>
2018-07-13 16:29:56 -0400

    converted show to todo

--- src/components/cards/show.js
+++ src/components/cards/show.js
@@ -3,27 +3,7 @@ import Component from '@reactions/component'
 import { get } from '../../lib/cards'
 import { Typography, withStyles } from '@material-ui/core'
 
-const Show = ({ match, classes }) => (
-  <Component
-    initialState={{ card: null }}
-    didMount={({ setState }) =>
-      get(match.params.id).then(card => setState({ card: card }))
-    }
-  >
-    {({ state }) => (
-      <React.Fragment>
-        {!state.card ? (
-          <div>Loading...</div>
-        ) : (
-          <div className={classes.root}>
-            <Typography variant="title">{state.card.term}</Typography>
-            <Typography variant="caption">{state.card.definition}</Typography>
-          </div>
-        )}
-      </React.Fragment>
-    )}
-  </Component>
-)
+const Show = ({ match, classes }) => <div>Todo Show</div>
 
 const styles = {
   root: {
